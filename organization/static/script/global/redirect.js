a6766ec5e7ff0bdeda0f01d65a9baf6f0c37e590
Iago <iagomolinavazquez@gmail.com>
2025-09-15 18:57:44 -0800

    Fix URL
     Changes to be committed:
    	modified:   organization/static/script/global/redirect.js

--- organization/static/script/global/redirect.js
+++ organization/static/script/global/redirect.js
@@ -10,5 +10,5 @@ export function redirectToOriginalWebsite() {
         nextUrl = "/";
     }
     console.log(nextUrl)
-    redirect(`${getAppName()}${nextUrl}`);
+    redirect(`/${getAppName()}${nextUrl}`);
 }
