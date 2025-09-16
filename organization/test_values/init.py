2d673b54fe62ac2f46a913316478a18dba714fe8
Iago <iagomolinavazquez@gmail.com>
2025-09-15 18:15:33 -0800

    Fix params 2
     Changes to be committed:
    	modified:   organization/test_values/init.py

--- organization/test_values/init.py
+++ organization/test_values/init.py
@@ -4,12 +4,12 @@ from organization.models import (Address, OrganizationBranch, PersonRoleType,
 from organization.test_values.utils import get_person_id
 from app.settings import ORG_MGMT_APP_EXAMPLE
 
-def create_models(model, records):
+def create_models(app, model, records):
     objects = [model(**record) for record in records]
     return model.objects.using(app).bulk_create(objects)
 
 def create_test_branches(app, addresses):
-    branches = create_models(OrganizationBranch, [
+    branches = create_models(app, OrganizationBranch, [
         {"address":addresses[0], "since":"2024-01-20"},
         {"address":addresses[1], "since":"2024-08-20"},
     ])
@@ -33,18 +33,18 @@ def create_test_persons(app, person_role_type):
             "surname"   :f"Persona{test_record_num + 1} apellido",
             "birth_date":f"1990-03-2{test_record_num}"  ,
         })
-    persons = create_models(Person, test_records)
+    persons = create_models(app, Person, test_records)
     half_persons_len = int(NUM_OF_TEST_PERSONS / 2)
     person_roles = []
     for test_record_num in range(0, half_persons_len):
         add_test_person_role(person_roles, persons[test_record_num], person_role_type[0]) # add test members
     for test_record_num in range(half_persons_len, NUM_OF_TEST_PERSONS):
         add_test_person_role(person_roles, persons[test_record_num], person_role_type[1]) # add test beneficiaries
-    create_models(PersonRole, person_roles)
+    create_models(app, PersonRole, person_roles)
     return persons
 
 def create_test_money_donations(currency, persons):
-    money_donations = create_models(MoneyDonation, [
+    money_donations = create_models(app, MoneyDonation, [
         {"person":persons[0], "currency":currency[0], "amount": 3000.00, "date":"2025-11-20"},
         {"person":persons[1], "currency":currency[1], "amount": 7000.00, "date":"2025-11-21"},
         {"person":persons[5], "currency":currency[0], "amount":28000.00, "date":"2025-11-22"},
@@ -56,21 +56,21 @@ def create_test_object(object_type):
     _object = Object.objects.using(ORG_MGMT_APP_EXAMPLE).create(type=object_type)
     return _object
 def create_test_object_donations(persons):
-    object_types = create_models(ObjectType, [
+    object_types = create_models(app, ObjectType, [
         {"value":"Buzo"},{"value":"Cama"},{"value":"Horno"}
     ])
-    object_donations = create_models(ObjectDonation, [
+    object_donations = create_models(app, ObjectDonation, [
         {"object":create_test_object(object_types[0]), "donor":persons[0], "date":"2024-07-20"},
         {"object":create_test_object(object_types[1]), "donor":persons[0], "date":"2024-07-21"},
         {"object":create_test_object(object_types[1]), "donor":persons[1], "date":"2024-07-21"},
         {"object":create_test_object(object_types[2]), "donor":persons[5], "date":"2024-07-20"},
     ])
-    object_pass_types = create_models(ObjectPassType, [
+    object_pass_types = create_models(app, ObjectPassType, [
         {"value":"Transferencia a beneficiario"},
         {"value":"Transferencia a organización"},
         {"value":"Perdido"                     },
     ])
-    object_owner_exchanges = create_models(ObjectPass, [
+    object_owner_exchanges = create_models(app, ObjectPass, [
         {"type":object_pass_types[0], "object":object_donations[0].object, "new_person":persons[6], "datetime":"2025-09-20 15:40:40"},
         {"type":object_pass_types[1], "object":object_donations[0].object, "new_person":persons[3], "datetime":"2025-11-21 17:45:45"},
         {"type":object_pass_types[0], "object":object_donations[1].object, "new_person":persons[8], "datetime":"2025-11-21 17:45:45"},
@@ -79,7 +79,7 @@ def create_test_object_donations(persons):
     return object_donations
 
 def create_test_expenditures(currency):
-    expenditures = create_models(Expenditure, [
+    expenditures = create_models(app, Expenditure, [
         {"date":"2024-05-28", "amount":10000.00, "currency":currency[0]},
         {"date":"2024-09-28", "amount":10000.00, "currency":currency[1]},
         {"date":"2024-11-28", "amount":32000.00, "currency":currency[0]},
@@ -91,17 +91,17 @@ def init_organization_test_values(app):
     if PersonRoleType.objects.using(app).exists():
         return
     if app == ORG_MGMT_APP_EXAMPLE:
-        addresses = create_models(Address, [
+        addresses = create_models(app, Address, [
             {"street_address1":"San Martin 2382", "city":"CABA", "state_province":"Buenos Aires", "country":"Argentina",},
             {"street_address1":"Yrigoyen 1395"  , "city":"CABA", "state_province":"Buenos Aires", "country":"Argentina",},
         ])
         create_test_branches(app, addresses)
-    person_role_type = create_models(PersonRoleType, [
+    person_role_type = create_models(app, PersonRoleType, [
         {"value":"Miembro"}, {"value":"Beneficiario"},
     ])
     if app == ORG_MGMT_APP_EXAMPLE:
         persons = create_test_persons(app, person_role_type)
-        currency = create_models(Currency, [{"code":"USD"}, {"code":"ARS"}])
+        currency = create_models(app, Currency, [{"code":"USD"}, {"code":"ARS"}])
         create_test_money_donations(app, currency, persons)
         create_test_object_donations(app, persons)
         create_test_expenditures(app, currency)
