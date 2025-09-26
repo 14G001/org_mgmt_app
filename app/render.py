27b7d2904f315b961bb38d7781c59d0a5040307e
Iago <iagomolinavazquez@gmail.com>
2025-09-25 19:56:45 -0800

    Update texts
     Changes to be committed:
    	modified:   app/render.py
    	modified:   organization/templates/admin.html

--- app/render.py
+++ app/render.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from app.settings import EXAMPLE_APP_INDICATOR
+from app.settings import EXAMPLE_APP_INDICATOR, ORG_MGMT_APP_EXA
 
 def send_template(request, app, path):
-    title = None
     is_example_app_version = app.endswith(EXAMPLE_APP_INDICATOR)
+    app_name = "Enseñá X Argentina" if app.startswith(ORG_MGMT_APP_EXA) else None
+    title = f"{app_name} Admin"
     if is_example_app_version:
-        title = "ONG Admin Example Version"
-    else:
-        title = "ONG Admin"
+        title += " - Versión de Ejemplo"
     return render(request, path, {
         "title":title, "example_version":is_example_app_version})
