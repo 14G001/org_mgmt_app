fec7ec2b92cea43675e0424f7aef7925aac8c2c2
Iago <iagomolinavazquez@gmail.com>
2025-09-26 15:36:48 -0800

    Fix app not exists screen
     Changes to be committed:
    	modified:   app/responses.py
    	modified:   app/settings.py

--- app/responses.py
+++ app/responses.py
@@ -1,9 +1,9 @@
-from django.http import JsonResponse, Http404
+from django.http import JsonResponse, HttpResponseNotFound
 
 def json_result(status=200, **kwargs):
     return JsonResponse(kwargs, status=status)
 def resource_not_exists():
-    return Http404("Resource not found.")
+    return HttpResponseNotFound("El recurso no existe.")
 def error(status, result_message, **kwargs):
     kwargs['result'] = result_message
     return json_result(status, **kwargs)
